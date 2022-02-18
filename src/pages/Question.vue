1e3629062a95f43bf3225d70c8888a7b48c11d7a
s325909 <jaso_cyril@hotmail.com>
2022-02-18 19:12:33 +0100

    Feat: print question, answer and wrong for each trivia question

--- src/pages/Question.vue
+++ src/pages/Question.vue
@@ -5,9 +5,18 @@ import { fetchAllQuestions } from "../api/questions"
 const difficulty = { easy: 'easy', medium: 'medium', hard: 'hard' };
 const type = { boolean: 'boolean', multiple: 'multiple'};
 
-const [error, questions] = await fetchAllQuestions(difficulty.easy, type.boolean);
+const [error, questions] = await fetchAllQuestions(difficulty.easy, type.multiple);
 console.log(questions);
 console.log(error);
+
+for (const trivia of questions) {
+    const { question, correct_answer, incorrect_answers } = trivia;
+
+    console.log("-----------------------------------------------");
+    console.log("QUESTION: " + question);
+    console.log("ANSWER: " + correct_answer);
+    console.log("WRONGS: " + incorrect_answers);
+}
 
 </script>
 
