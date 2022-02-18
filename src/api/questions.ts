6edb353aff50f279dfcfa687ea207a44a6762032
s325909 <jaso_cyril@hotmail.com>
2022-02-18 18:44:32 +0100

    Feat: fetch all from trivia api

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -1,1 +1,50 @@
-import { BASE_URL } from "./";
+import axios from "axios";
+import { QUIZ_URL } from "./";
+
+const TRIVIA_URL = "https://opentdb.com/api.php?amount=10&category=15"
+
+const apiDiff = "&difficulty=";
+const apiType = "&type=";
+
+let difficulty = "";
+let type = "";
+
+
+export interface TriviaResponse {
+    response_code: number,
+    results: Trivia[]
+}
+
+export interface Trivia {
+    category: string,
+    type: string,
+    difficulty: string,
+    question: string,
+    correct_answer: string,
+    incorrect_answers: string[]
+}
+
+
+export async function fetchAllQuestions(): Promise<[string | null, Trivia[]]> {
+    try {
+        const { data } = await axios.get<TriviaResponse>(TRIVIA_URL);
+		return [null, data.results];
+    } catch (error: any) {
+        return [error.message, []];
+    }
+}
+
+
+
+
+
+export function selectDifficulty( d: string ) {
+    difficulty = apiDiff + d;
+    console.log("API DIFFICULTY: " + difficulty);
+}
+
+export function selectType( t: string ) {
+    type = apiType + t;
+    console.log("API Type: " + type);
+}
+
--- src/pages/Question.vue
+++ src/pages/Question.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import { fetchAllQuestions, selectDifficulty, selectType } from "../api/questions"
+
+
+const difficulty = { easy: 'easy', medium: 'medium', hard: 'hard' };
+const type = { boolean: 'boolean', multiple: 'multiple'};
+
+
+selectDifficulty(difficulty.hard);
+
+selectType(type.boolean);
+
+const [_error, _questions] = await fetchAllQuestions();
+console.log(_questions);
+console.log(_error);
+
+</script>
+
 <template>
-<h1>Hello Question</h1>
+    <h1>Hello Question</h1>
 </template>
