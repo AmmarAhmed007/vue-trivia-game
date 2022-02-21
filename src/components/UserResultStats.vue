f187d9981f96e0d9c67f008f7b8e73236b4a6c5a
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-02-21 21:27:47 +0100

    FEAT: Added UserResultStats components and template for Userstats in result

---
+++ src/components/UserResultStats.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import store from '../store';
+import { computed, reactive, ref } from "vue";
+
+
+function listUserResult() {
+
+
+    questions.value.forEach(element => {
+        
+    });
+}
+
+    const questions = computed(() => store.state.questions);
+    const answers = computed(() => store.state.answers);
+
+</script>
+
+<template>
+    <ul>
+        <p v-for="question in questions" :key="question">
+            {{question}}
+        </p>
+        <li v-for="answer in answers" :key="answer">
+            {{answer}}
+        </li>
+    </ul>
+</template>
--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -1,47 +1,76 @@
 <script setup lang="ts">
-    import 'animate.css'
-    import { computed, reactive, ref } from "vue";
-    import { useStore } from "vuex";
+import 'animate.css'
+import { computed, reactive, ref } from "vue";
+import { useStore } from "vuex";
+import UserResultStats from '../components/UserResultStats.vue'
 
-    const store = useStore();
+const store = useStore();
 
-    const questions = computed(() => store.state.questions);
-    const questionAnswers = computed(() => store.state.answers);
-    const userAnswers = computed(() => store.state.results);
+const questions = computed(() => store.state.questions);
+const questionAnswers = computed(() => store.state.answers);
+const userAnswers = computed(() => store.state.results);
 
-    console.log(questions.value);
-    console.log(questionAnswers.value);
-    console.log(userAnswers.value);
+console.log(questions.value);
+console.log(questionAnswers.value);
+console.log(userAnswers.value);
+
+function listUserStat() {
+    
+}
 
 </script>
 
 <template>
-    <div class="result shadow-lg shadow-black">
-        <div class="header animate__animated animate__tada">
-            <h2>High Score</h2>
+    <div class="main">
+        <div class="result shadow-lg shadow-black">
+            <div class="header animate__animated animate__tada">
+                <h2>High Scores</h2>
+            </div>
+
+            <div class="grid-container">
+                <div class="grid-item username animate__animated animate__bounceInLeft">Username</div>
+                <div class="grid-item score animate__animated animate__bounceInLeft">Score</div>
+                <!-- <table>
+                    <tr>
+                        <th class="username animate__animated animate__bounceInLeft">Username</th>
+                        <th class="score animate__animated animate__bounceInLeft">Score</th>
+                    </tr>
+                </table> -->
+            </div>
         </div>
-        <div class="grid-container">
-            <div class="username animate__animated animate__bounceInLeft">Username</div>
-            <div class="score animate__animated animate__bounceInLeft">Score</div>
-            <p>{{questions}}</p> <br>
-            <p>{{questionAnswers}}</p> <br>
-            <p>{{userAnswers}}</p>
+
+        <div class="result shadow-lg shadow-black">
+            <div class="header">
+                <h2>Your Stats</h2>
+                <UserResultStats />
+                <!-- <p>{{ questions }}</p>
+                <br />
+                <p>{{ questionAnswers }}</p>
+                <br />
+                <p>{{ userAnswers }}</p> -->
+            </div>
+        </div>
+
+        <div>
+            <router-link to="/">
+                <button class="btn1 btn" type="submit">Back to start</button>
+            </router-link>
+            <router-link to="/question">
+                <button class="btn2 btn" type="submit">Replay</button>
+            </router-link>
         </div>
     </div>
-            <div>
-                <router-link to="/">
-                    <button class="btn1 btn" type="submit">Back to start</button>
-                </router-link>
-                <router-link to="/question">
-                    <button class="btn2 btn" type="submit">Replay</button>
-                </router-link>
-            </div>
 </template>
 
 <style>
+* {
+    box-sizing: border-box;
+}
+
 .grid-container {
     display: grid;
     text-align: center;
+    float: left;
 }
 
 .username {
@@ -58,17 +87,12 @@
     width: 60vh;
     height: 60vh;
     padding: 10px;
+    float: left;
+    margin: 30px;
 }
 
 .header {
     text-align: center;
-}
-
-table {
-    padding: 15px;
-    border: 1px solid;
-    text-align: center;
-    align-content: center;
 }
 
 .btn1 {
@@ -77,5 +101,9 @@ table {
 
 .btn2 {
     grid-column: 2;
+}
+
+.main {
+    display: flex;
 }
 </style>
